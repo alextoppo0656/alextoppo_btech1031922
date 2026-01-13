3b268c3e8eddd82ab0f01af4962281c6a654e02f
zekiruszirax <zekiruszirax@gmail.com>
2026-01-13 16:41:27 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,8 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:3000",
         "https://alextoppo-btech1031922.vercel.app/",
-        "https://*.vercel.app"  # Allow all Vercel preview URLs
+        "https://*.vercel.app",
+        "*"  # Allow all Vercel preview URLs
     ],
     allow_credentials=True,
     allow_methods=["*"],
